ab7da9630e7f0c35bc241de14615da4e2699a028
Seth Pollack <seth@pollackphoto.com>
2012-02-09 10:16:55 -0800

    take out NWAC user agent hack

--- AviForecastIOS/MainViewController.m
+++ AviForecastIOS/MainViewController.m
@@ -371,11 +371,6 @@
     // load the regions and forecasts
     [self loadData];
     
-    // NOTE set our user agent string to something benign and non-mobile looking, to work around website
-    // popups from nwac.us asking if you would like to be redirected to the mobile version of the site
-    NSDictionary * dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Mozilla/5.0", @"UserAgent", nil];
-    [[NSUserDefaults standardUserDefaults] registerDefaults:dictionary];  
-    
     // register so that on app re-entering the foreground, we update our forecasts
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateAllForecastData:) name:UIApplicationWillEnterForegroundNotification object:nil];
 }
