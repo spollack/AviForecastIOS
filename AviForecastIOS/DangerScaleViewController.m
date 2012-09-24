62286fd28b96a3f655ed200eb966db8095d0ed6c
Seth Pollack <seth@pollackphoto.com>
2012-09-23 19:43:48 -0700

    larger danger scale image for iPhone5 support; allow zooming in danger scale view

--- AviForecastIOS/DangerScaleViewController.m
+++ AviForecastIOS/DangerScaleViewController.m
@@ -8,14 +8,116 @@
 
 #import "DangerScaleViewController.h"
 
+@interface DangerScaleViewController ()
+@property (nonatomic, strong) UIImageView *imageView;
+
+- (void)centerScrollViewContents;
+- (void)scrollViewDoubleTapped:(UITapGestureRecognizer*)recognizer;
+- (void)scrollViewTwoFingerTapped:(UITapGestureRecognizer*)recognizer;
+@end
+
 @implementation DangerScaleViewController
 
 @synthesize delegate = _delegate;
 @synthesize scrollView = _scrollView;
 
+@synthesize imageView = _imageView;
+
+- (void)centerScrollViewContents
+{
+    CGSize boundsSize = self.scrollView.bounds.size;
+    CGRect contentsFrame = self.imageView.frame;
+    
+    if (contentsFrame.size.width < boundsSize.width) {
+        contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0f;
+    } else {
+        contentsFrame.origin.x = 0.0f;
+    }
+    
+    if (contentsFrame.size.height < boundsSize.height) {
+        contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0f;
+    } else {
+        contentsFrame.origin.y = 0.0f;
+    }
+    
+    self.imageView.frame = contentsFrame;
+}
+
+- (void)scrollViewDoubleTapped:(UITapGestureRecognizer*)recognizer
+{
+
+    CGPoint pointInView = [recognizer locationInView:self.imageView];
+    
+    CGFloat newZoomScale = self.scrollView.zoomScale * 1.5f;
+    newZoomScale = MIN(newZoomScale, self.scrollView.maximumZoomScale);
+    
+    CGSize scrollViewSize = self.scrollView.bounds.size;
+    
+    CGFloat w = scrollViewSize.width / newZoomScale;
+    CGFloat h = scrollViewSize.height / newZoomScale;
+    CGFloat x = pointInView.x - (w / 2.0f);
+    CGFloat y = pointInView.y - (h / 2.0f);
+    
+    CGRect rectToZoomTo = CGRectMake(x, y, w, h);
+    
+    [self.scrollView zoomToRect:rectToZoomTo animated:YES];
+}
+
+- (void)scrollViewTwoFingerTapped:(UITapGestureRecognizer*)recognizer
+{
+    // zoom out slightly, capping at the minimum zoom scale specified by the scroll view
+    CGFloat newZoomScale = self.scrollView.zoomScale / 1.5f;
+    newZoomScale = MAX(newZoomScale, self.scrollView.minimumZoomScale);
+    [self.scrollView setZoomScale:newZoomScale animated:YES];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIImage * image = [UIImage imageNamed:@"dangerScaleFrontLarge.jpg"];
+    self.imageView = [[UIImageView alloc] initWithImage:image];
+    self.imageView.frame = (CGRect){.origin=CGPointMake(0.0f, 0.0f), .size=image.size};
+    [self.scrollView addSubview:self.imageView];
+    
+    self.scrollView.contentSize = image.size;
+    
+    UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewDoubleTapped:)];
+    doubleTapRecognizer.numberOfTapsRequired = 2;
+    doubleTapRecognizer.numberOfTouchesRequired = 1;
+    [self.scrollView addGestureRecognizer:doubleTapRecognizer];
+    
+    UITapGestureRecognizer *twoFingerTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewTwoFingerTapped:)];
+    twoFingerTapRecognizer.numberOfTapsRequired = 1;
+    twoFingerTapRecognizer.numberOfTouchesRequired = 2;
+    [self.scrollView addGestureRecognizer:twoFingerTapRecognizer];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    CGRect scrollViewFrame = self.scrollView.frame;
+    CGFloat scaleWidth = scrollViewFrame.size.width / self.scrollView.contentSize.width;
+    CGFloat scaleHeight = scrollViewFrame.size.height / self.scrollView.contentSize.height;
+    CGFloat minScale = MIN(scaleWidth, scaleHeight);
+    self.scrollView.minimumZoomScale = minScale;
+    
+    self.scrollView.maximumZoomScale = 1.0f;
+    self.scrollView.zoomScale = scaleHeight;
+    
+    [self centerScrollViewContents];
+}
+
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
 {
-    return [self.scrollView.subviews objectAtIndex:0];
+    return self.imageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    // The scroll view has zoomed, so you need to re-center the contents
+    [self centerScrollViewContents];
 }
 
 - (IBAction)donePressed:(id)sender
@@ -24,20 +126,12 @@
     [self.delegate dangerScaleViewControllerDidFinish:self];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    // set the content size of the scroll view based on the image size
-    CGSize imageSize = ((UIImageView *)[self.scrollView.subviews objectAtIndex:0]).image.size;
-    self.scrollView.contentSize = imageSize;
-}
-
 - (void)viewDidUnload
 {
     [self setDelegate:nil];
     [self.scrollView setDelegate:nil];
     [self setScrollView:nil];
+    [self setImageView:nil];
     
     [super viewDidUnload];
 }
